813498d0c294abfd865920cf8607d9a97ef74829
Bishal Neupane <neupanebishal07@gmail.com>
2021-11-14 11:42:25 +0545

    Update port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import app from "./server";
 
 const main = async () => {
   try {
-    await app.listen(process.env.PORT || 4000);
+    await app.listen(process.env.PORT || 4000, "0.0.0.0");
   } catch (error) {
     app.log.error(error);
     process.exit(1);
